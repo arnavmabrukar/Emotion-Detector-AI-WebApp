aaac0e1fbfa8e55a08d951e4d60b168472ada053
arnavmabrukar <arnm1077@gmail.com>
2026-03-05 00:26:19 -0500

    Completed packaging function and works well

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,26 @@
+import json, requests
+
+def emotion_detector(text_to_analyze):
+
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    myobj = { "raw_document": { "text": text_to_analyze } }
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+
+    response = requests.post(url, json=myobj, headers=header)
+    data = json.loads(response.text)
+    emotions = data["emotionPredictions"][0]["emotion"]
+
+    top_emotion = max(emotions, key=emotions.get)
+
+    result = {
+
+        'anger': emotions['anger'],
+        'disgust': emotions['disgust'],
+        'fear': emotions['fear'],
+        'joy': emotions['joy'],
+        'sadness': emotions['sadness'],
+        'dominant_emotion': top_emotion
+        
+        }
+
+    return result
